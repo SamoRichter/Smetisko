ad8b6c334b097367fcb6f8ca83508ad94855a2b0
Richter <samuel.richter@piaristi-pd.sk>
2024-10-17 07:41:44 +0200

    ddddd

---
+++ PHP/05_skladame_stranku_include/assets/partials/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Moja Webová Stránka</h1>
+        <nav>
+            <ul>
+                <li><a href="index.html">Úvod</a></li>
+                <li><a href="blog.html">Blog</a></li>
+                <li><a href="o-mne.html">O mne</a></li>
+                <li><a href="kontakt.html">Kontakt</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main></main>
