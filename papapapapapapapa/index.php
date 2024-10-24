138ca7a7747e5b03471552f0defd188b113fc2b7
Richter <samuel.richter@piaristi-pd.sk>
2024-10-24 08:03:39 +0200

    dd

--- papapapapapapapa/index.php
+++ papapapapapapapa/index.php
@@ -8,25 +8,27 @@
 <body>
     <?php
     $jeden = 5;
-    $jeden2 = 8;
+    $jeden2 = 55;
     $jeden3 = 12;
 
-    if ($jeden > $jeden2 && $jeden > $jeden3) {
-     echo "cislo " . "$jeden " .  "je najvacsie"; }
+    if ($jeden >= $jeden2 && $jeden >= $jeden3) {
+        echo "cislo " . "$jeden " .  "je najvacsie"; }
 
-    if ($jeden2 > $jeden && $jeden2 > $jeden3) {
-     echo "cislo " . "$jeden2 " .  " je najvacsie"; }
+    if ($jeden2 >= $jeden && $jeden2 >= $jeden3) {
+        echo "cislo " . "$jeden2 " .  " je najvacsie"; }
 
-    if ($jeden3 > $jeden && $jeden3 > $jeden2) {
-     echo "cislo " . "$jeden3 " .  " je najvacsie"; }
+    if ($jeden3 >= $jeden && $jeden3 >= $jeden2) {
+        echo "cislo " . "$jeden3 " .  " je najvacsie"; }
 
-     if ($jeden < $jeden2 && $jeden < $jeden3) {
+        echo"<br>";
+
+    if ($jeden <= $jeden2 && $jeden <= $jeden3) {
         echo "cislo " . "$jeden " .  "je najmensie"; }
    
-       if ($jeden2 < $jeden && $jeden2 < $jeden3) {
+    if ($jeden2 <= $jeden && $jeden2 <= $jeden3) {
         echo "cislo " . "$jeden2 " .  " je najmensie"; }
    
-       if ($jeden3 < $jeden && $jeden3 < $jeden2) {
+    if ($jeden3 <= $jeden && $jeden3 <= $jeden2) {
         echo "cislo " . "$jeden3 " .  " je najmensie"; }
    
 
