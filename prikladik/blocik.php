da5597ca4466690c8f864889b7330cb3d0ebc6b9
Richter <samuel.richter@piaristi-pd.sk>
2024-11-07 09:49:26 +0100

    uuu

--- prikladik/blocik.php
+++ prikladik/blocik.php
@@ -4,17 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .tablet {
+            background-color: ;
+        }
+    </style>
 </head>
 <body>
     <?php
-
+/*
         $nakup = ["krumple", "mrkvicky", "rajcinky", "tatranka", "slivovica", "cipsy", "voda", "vajca"];
         $cena = [1.5 , 1.3 , 1.4, 0.5, 5, 1, 0.7 , 1.8 ];
-    /*
+    
         for ($i = 0; $i < count($nakup); $i++) {
             echo $nakup[$i] ;
         }
-        */
+        
         echo"<pre>";
         echo print_r( $nakup );
         echo"<pre>";
@@ -25,6 +30,15 @@
 
         unset( $nakup[3], $cena[3] );
 
+
+        array_push($nakup, "lego");
+        array_push($cena, "15");
+
+        array_unshift($nakup, "chleba" );
+        array_unshift($cena, "1.4" );
+        
+
+        
         echo"<pre>";
         echo print_r( $nakup );
         echo"<pre>";
@@ -33,12 +47,24 @@
         echo print_r( $cena );
         echo"<pre>";
 
-        array_push($nakup, "lego");
-        array_push($cena, "15");
+        $pocet = count($nakup);
 
 
 
+        
+        ;
+        for ($i = 0; $i < $pocet; $i++) {
+            echo '<table>';
+            echo '<tr>';
+            echo "<td>" . $nakup[$i] . "   " . "</td>"  ;  
+            echo "<td>" . $cena[$i] . "€" . "</td>" ;
+            echo "</tr>";
+            echo "</table>";
 
+        }
+*/
+        
+        
 
 
     ?>
